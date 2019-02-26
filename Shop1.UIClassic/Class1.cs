445c2c6e3ba94551b47d82267111562fa751a744
yolyr <yolyr@DESKTOP-7AK4SSQ>
2019-02-25 19:23:13 -0500

    Add project files.

---
+++ Shop1.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shop1.UIClassic
+{
+    public class Class1
+    {
+    }
+}
---
+++ Shop1.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shop1.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
